229224cee768eb7122c1feacfbfd16d7204d392f
Jie-PC\Jie <jiex312@gmail.com>
2017-09-15 16:20:13 +0900

    カメラ管理用script作成

--- Cosmic_Life/Assets/Chok/Camera/CameraManager.cs
+++ Cosmic_Life/Assets/Chok/Camera/CameraManager.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField,Tooltip("カメラリスト")]
+    private List<GameObject> m_cameraList;
 
-    // Use this for initialization
-    void Start()
+    private int m_currentCamera = 0;
+    
+    public IEnumerator SwitchCamera(int num)
     {
-        Debug.Log(Display.displays.Length);
-        //Display.displays[1].Activate();
-        PlayerPrefs.SetInt("UnitySelectMonitor", 1);
-
+        // フェードアウト
+        FadeMgr.Instance.FadeOut(0.5f);
+        // 0.5秒待つ
+        yield return new WaitForSeconds(0.5f);
+        // 今使っているカメラを非アクティブする
+        m_cameraList[m_currentCamera].SetActive(false);
+        // 次のカメラに移行
+        m_currentCamera += num;
+        if (m_currentCamera >= m_cameraList.Count) m_currentCamera %= m_cameraList.Count;
+        if (m_currentCamera < 0) m_currentCamera = m_cameraList.Count - 1;
+        // 新しいカメラをアクティブする
+        m_cameraList[m_currentCamera].SetActive(true);
+        // フェードイン
+        FadeMgr.Instance.FadeIn(0.5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddCamera(GameObject camera)
     {
+        m_cameraList.Add(camera);
+    }
+
+    public void EraseCamera(GameObject camera)
+    {
+        foreach(var c in m_cameraList)
+        {
+            if (camera != c) continue;
+            m_cameraList.Remove(camera);
+            break;
+        }
     }
 }
